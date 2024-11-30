05d6ef019ae44679ae670ee1475209c8e0a4afde
Robert Antonius <robert.antonius23@gmail.com>
2024-11-30 22:39:03 +0700

    Funky base shenanigans

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// https://dev.to/rashidshamloo/deploying-vite-react-app-to-github-pages-35hf
 export default defineConfig({
   plugins: [react()],
+  base: "/roberika.github.io"
 })
