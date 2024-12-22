77fa5081722790ffc9d83473e892543fe93716e7
Robert Antonius <robert.antonius23@gmail.com>
2024-12-23 02:45:28 +0700

    Classic

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://dev.to/rashidshamloo/deploying-vite-react-app-to-github-pages-35hf
 export default defineConfig({
   plugins: [react()],
-  base: "/roberika.github.io"
+  base: "/"
 })
